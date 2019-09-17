8ce184c506051012b19e917f117b61915dc329b1
Gauri Dandi <gauri_dandi@Gauris-MacBook-Pro.local>
2019-09-17 10:53:19 -0400

    Homework 2

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
 	let mySrc = myImage.getAttribute('src');
-	if (mySrc === "/Users/gauri_dandi/Documents/Northeastern/2019-2020/DS4200/Homeworks/H1/homework-assignment-1-gdandi04/images/city-picture.jpeg") {
-		myImage.setAttribute('src', "/Users/gauri_dandi/Documents/Northeastern/2019-2020/DS4200/Homeworks/H1/homework-assignment-1-gdandi04/images/city2.jpeg");
+	if (mySrc === "images/city-picture.jpeg") {
+		myImage.setAttribute('src', "images/city2.jpeg");
 	} else {
-		myImage.setAttribute('src', "/Users/gauri_dandi/Documents/Northeastern/2019-2020/DS4200/Homeworks/H1/homework-assignment-1-gdandi04/images/city-picture.jpeg");
+		myImage.setAttribute('src', "images/city-picture.jpeg");
 	}
 }
 
